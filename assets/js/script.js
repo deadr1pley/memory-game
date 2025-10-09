9f1893740c39fd45736215c3dcdccde0cb2d8b02
deadr1pley <vanmatrix@hotmail.com>
2025-10-09 18:41:14 +0200

    Add function in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const board = document.getElementById("board");
+const scoreDisplay = document.getElementById("score");
+
+function startGame() {
+
+}
+
+function drawBoard() {
+
+}
+
+function flipCard() {
+
+}
+
+function CheckMatch() {
+
+}
+
+function updateScore() {
+    
+}
